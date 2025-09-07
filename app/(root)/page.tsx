8270e5f7beb110a19028fa43b1a3d9415a212e65
AshrafShuvo022 <ashraf.shuvo022@gmail.com>
2025-09-07 13:22:06 +0600

    feat: implement structure of StartupCard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
 import Image from "next/image";
 
+export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
+  const query = (await searchParams).query;
 
-
-export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>
-}) {
-  const query = (await searchParams).query   ;
+  const posts = [{
+    _createdAt: new Date() ,
+    views: 55,
+    author: {_id: 1 , name: 'Ashraf Shuvo'},
+    _id: 1 ,
+    description: "This is a description",
+    image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8f&auto=format&fit=crop&w=1170&q=80",
+    category: "Robots",
+    title: "We Robots",
+  },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -13,9 +24,26 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
           Pitch Your Startup, <br /> Connect with Entrepreneurs
         </h1>
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches and Get Noticed  in virtural Competitions
+          Submit Ideas, Vote on Pitches and Get Noticed in virtural Competitions
         </p>
-        <SearchForm query ={query} /> 
+        <SearchForm query={query} /> 
+      </section>
+      <section className="selection_container">
+         <p className="text-30-semibold ">
+           {query ? `Search results for "${query}"` : 'All Startups'}
+         </p>
+         <ul className="mt-7 card_grid">
+          {
+            posts?.length > 0 ? (
+              posts.map((post: StartupTypeCard, index: number) => (
+                <StartupCard key ={post?._id} post = {post}/>
+              ))
+            )
+           : (
+            <p className="no-results" > No startups found</p>
+          )
+        }
+         </ul>
       </section>
     </>
   );
