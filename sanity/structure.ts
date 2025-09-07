b50b8e36597bb5dbf8868bd8d20d7b6040a5b473
AshrafShuvo022 <ashraf.shuvo022@gmail.com>
2025-09-07 15:31:47 +0600

    correctly added documentTypeListItem

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -5,6 +5,7 @@ export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
     .items([
-      S.documentTypeListItem("author").title("Authors")])
-       .items([
-      S.documentTypeListItem("startup").title("startups")])
+      S.documentTypeListItem("author").title("Authors"),
+      S.documentTypeListItem("startup").title("Startups")
+    ])
+       
