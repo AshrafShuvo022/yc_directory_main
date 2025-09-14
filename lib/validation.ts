fa8a2d26d407bf486d530f96d4345d3ffbd56b21
AshrafShuvo022 <ashraf.shuvo022@gmail.com>
2025-09-15 04:40:58 +0600

    solved image problem

--- lib/validation.ts
+++ lib/validation.ts
@@ -5,17 +5,16 @@ export const formSchema = z.object({
   description: z.string().min(20).max(500),
   category: z.string().min(3).max(20),
   link: z
-    .string()
-    .url()
-    .refine(async (url) => {
-      try {
-        const res = await fetch(url, { method: "HEAD" });
-        const contentType = res.headers.get("content-type");
+  .string()
+  .url("Must be a valid URL")
+  .refine((url) => {
+    // Normal image extensions
+    if (/\.(jpeg|jpg|png|gif|webp|svg)$/i.test(url)) return true;
 
-        return contentType?.startsWith("image/");
-      } catch {
-        return false;
-      }
-    }),
+    // Unsplash dynamic links
+    if (url.includes("source.unsplash.com")) return true;
+
+    return false;
+  }, "Must be an image URL"),
   pitch: z.string().min(10),
 });
